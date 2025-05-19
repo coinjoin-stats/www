003b3cba9396a2e1923086124c8312d5671669e7
jirigav <jirigavenda98@gmail.com>
2025-05-19 12:56:09 +0200

    add favicon

--- build.py
+++ build.py
@@ -25,6 +25,7 @@ def get_header(structure, current_page):
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <title>CoinJoin Statistics</title>
     <link rel="stylesheet" href="style.css?v1" />
+    <link rel="icon" type="image/x-icon" href="favicon.ico">
   </head>
   <body>
       <header>
